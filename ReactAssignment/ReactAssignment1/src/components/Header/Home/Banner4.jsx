22409e6d6976e0863a596ddd8a953dfa2a4d13a8
dhawal-haptiq <dhawal.wani@haptiq.com>
2025-08-08 20:24:28 +0530

    solved conflicts

--- ReactAssignment/ReactAssignment1/src/components/Header/Home/Banner4.jsx
+++ ReactAssignment/ReactAssignment1/src/components/Header/Home/Banner4.jsx
@@ -7,7 +7,7 @@ const Banner4 = () => {
       <div className="relative bg-[url('https://www.chanel.com/puls-img/c_limit,w_3200/q_auto:good,dpr_auto,f_auto/1742562117322-homepagecorpoonedesktopjpg_3240x5760.jpg')] bg-cover bg-center h-screen flex items-end justify-end">
         <div className="flex flex-col items-end px-12 pb-12 text-right text-black font-light tracking-widest ">
           <p className=" tracking-widest mb-2">FASHION</p>
-          <h3 className='text-5xl'>THE WOMEN BAGS</h3>
+          <h3 className='text-5xl'>THE WOMENS BAGS</h3>
           <Link
             to="/category/womens-bags"
             className="mt-5 border border-black text-black text-sm px-6 py-3 tracking-widest hover:bg-black hover:text-white transition-colors duration-300"
